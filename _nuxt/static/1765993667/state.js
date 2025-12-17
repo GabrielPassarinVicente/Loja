f9ec2a2af6f74ef052b99f62e19b0db477609325
GabrielPassarinVicente <112556559+GabrielPassarinVicente@users.noreply.github.com>
2025-12-17 14:49:03 -0300

    Updates

--- _nuxt/static/1765993667/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftech\u002F_nuxt\u002Fstatic\u002F1765993667",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftech\u002F",assetsPath:"\u002Ftech\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1765993739/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontato","\u002Fportfolio","\u002Fprodutos","\u002Fservicos"]})
