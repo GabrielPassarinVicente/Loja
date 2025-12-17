c8071c21baac90199213e7471b5a1d7416a1944b
GabrielPassarinVicente <112556559+GabrielPassarinVicente@users.noreply.github.com>
2025-12-17 14:47:57 -0300

    Updates

---
+++ _nuxt/static/1765993667/produtos/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftech\u002F_nuxt\u002Fstatic\u002F1765993667",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprodutos",config:{_app:{basePath:"\u002Ftech\u002F",assetsPath:"\u002Ftech\u002F_nuxt\u002F",cdnURL:a}}}}(null));
